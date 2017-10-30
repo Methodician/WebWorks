ea136b0e780b88e619f95f3d5eaafe2da1033b22
Methodician <methodician@gmail.com>
2017-10-30 08:50:05 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { LoginComponent } from './components/login/login.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
+    //  AngularMaterial imports:
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule, 
